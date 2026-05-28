d722e846985e2f149724cba2a4d9dbb683d1635b
aayushi-raj1444 <aayushi.r5050@gmail.com>
2026-05-28 12:42:16 +0530

    Add files via upload

---
+++ tut44.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+template <class t>
+class vector{
+    public:
+    t *arr;//must
+    int size;
+    vector (int m){
+        size = m;
+        arr =new t[size];//key word new allocates a contiguous block of memory for multiple elements.
+    }
+    t dotproduct(vector &v){
+        t d =0;
+        for (int i=0;i<size;i++){
+            d+= this->arr[i]* v.arr[i];
+            //this keyword points to the specific object instance for which a non-static member function is being execute
+        }return d;
+    }
+};
+
+int main(){
+    vector<float> v1(3); //vector 1 with a float data type
+    v1.arr[0] = 1.4;
+    v1.arr[1] = 3.3;
+    v1.arr[2] = 0.1;
+    vector<float> v2(3); //vector 2 with a float data type
+    v2.arr[0]=0.1;
+    v2.arr[1]=1.90;
+    v2.arr[2]=4.1;
+    float a = v1.dotproduct(v2);
+    cout<<a<<endl;
+    return 0;
+}
